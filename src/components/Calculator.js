4a4a7dfbff3870368e80581641b1802ff292181e
harmolipi <nikob381@gmail.com>
2022-01-12 17:03:23 -0500

    Basic layout

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,6 +2,29 @@ import Display from './Display';
 import NumberButton from './NumberButton';
 import OperatorButton from './OperatorButton';
 
-const Calculator = () => {};
+const Calculator = () => {
+  return (
+    <div className="calculator">
+      <Display />
+      <div className="calculator-buttons grid grid-cols-4 grid-rows-5">
+        <NumberButton value="7" number="7" />
+        <NumberButton value="8" number="8" />
+        <NumberButton value="9" number="9" />
+        <OperatorButton value="+" />
+        <NumberButton value="4" number="4" />
+        <NumberButton value="5" number="5" />
+        <NumberButton value="6" number="6" />
+        <OperatorButton value="-" />
+        <NumberButton value="1" number="1" />
+        <NumberButton value="2" number="2" />
+        <NumberButton value="3" number="3" />
+        <OperatorButton value="*" />
+        <NumberButton value="0" number="0" />
+        <OperatorButton value="=" />
+        <OperatorButton value="/" />
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
--- src/components/NumberButton.js
+++ src/components/NumberButton.js
@@ -1,3 +1,22 @@
-const NumberButton = () => {};
+const NumberButton = ({ number }) => {
+  const numberMap = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    0: 'zero',
+  };
+
+  return (
+    <button id={numberMap[number]} className="number-button">
+      {number}
+    </button>
+  );
+};
 
 export default NumberButton;
